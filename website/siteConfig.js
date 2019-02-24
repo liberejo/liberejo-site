f62d62bcef3debf8b86224e93c132ce2e24104c5
Cassaundra <cass@cassaundra.io>
2019-02-24 11:39:21 -0800

    Update siteConfig for GitHub pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'Liberejo', // Title for your website.
+  tagline: 'The multiplayer sandbox game designed specificalyl for modding.',
+  url: 'https://liberejo.org', // Your website URL
+  baseUrl: '/liberejo-site', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'liberejo-site',
+  organizationName: 'liberejo',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
