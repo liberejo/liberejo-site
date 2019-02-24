86c5c6ae0051c46bdb612b313ec152d1adb24c20
Cassaundra <cass@cassaundra.io>
2019-02-24 13:11:48 -0800

    Update footer

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,6 +32,7 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'liberejo-site',
   organizationName: 'liberejo',
+  repoUrl: "https://github.com/liberejo/liberejo",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
