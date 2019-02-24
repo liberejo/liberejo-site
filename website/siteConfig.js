d840de5f0464119d86aa39cff9c08b2039e933d9
Cassaundra <cass@cassaundra.io>
2019-02-24 12:10:28 -0800

    Remove copyright, fix baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Liberejo', // Title for your website.
   tagline: 'The multiplayer sandbox game designed for modding.',
   url: 'https://liberejo.org', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/liberejo-site', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: '',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
